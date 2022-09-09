0cb4918e7ffd77cca687cc7bef283e9fa77700a5
Amandisen <manda.lindqvist@gmail.com>
2022-09-09 12:33:27 +0200

    Balloon collision detection, confetti prefab test

---
+++ main_balloon/Assets/Scripts/BalloonPopping.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BalloonPopping : MonoBehaviour
+{
+
+    public GameObject confettiExplosionPrefab;
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.name == "target")
+        {
+            PopBalloon();
+        }
+
+    }
+
+    private void PopBalloon()
+    {
+        GameObject confettiExplosion = Instantiate(confettiExplosionPrefab, gameObject.transform.position, confettiExplosionPrefab.transform.rotation);
+        Destroy(confettiExplosion, 1f);
+            
+        Destroy(gameObject);
+    }
+}
