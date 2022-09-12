1b74b6a2457d3f3f921b90f3ffc3139531d147e3
Agshu <agnes.shutrick@gmail.com>
2022-09-12 17:39:45 +0200

    Balloon movement upwards
    Realistic balloon movement upwards

--- main_balloon/Assets/Scripts/BalloonFloatingBehavior.cs
+++ main_balloon/Assets/Scripts/BalloonFloatingBehavior.cs
@@ -6,47 +6,32 @@ using UnityEngine;
 
 public class BalloonFloatingBehavior : MonoBehaviour
 {
-
-    // public Transform target;
-    public GameObject targetGO;
     public Rigidbody rb;
-    //public float amplitude = 0.1f;
-    // public float frequency = 1f;
-    public float force;
-
+    public float force = 1;
     public float inflationRate = 1.1f;
     public float maxBalloonScale = 1.0f;
+    public float height = 5;
 
-
-    //Vector3 posOffset = new Vector3 ();
-    //Vector3 tempPos = new Vector3 ();
+    Vector3 setHeight;
+    Vector3 heightVector;
 
     void Start()
     {
-        // Store the starting position of the object
-        //posOffset = transform.position;
         rb = gameObject.GetComponent<Rigidbody>();
-
+        setHeight = new Vector3(0, height, 0);
+        heightVector = transform.position + setHeight;
+       
     }
 
     void FixedUpdate()
     {
-        //rb.AddForce(transform.up * 1, ForceMode.Acceleration);
-
-        // Float up/down with a Sin()
-        //tempPos = posOffset;
-        //tempPos.y += 10;
-        // tempPos.y += Mathf.Sin (Time.fixedTime * Mathf.PI * frequency) * amplitude;
-        //transform.position = tempPos;
-
         if (transform.localScale.x < maxBalloonScale)
         {
             transform.localScale += transform.localScale * inflationRate * Time.deltaTime;
         }
 
-        targetGO = GameObject.Find("target");
 
-        Vector3 f = targetGO.transform.position - transform.position;
+        Vector3 f = heightVector - transform.position;
         f = f.normalized;
         f = f * force;
         rb.AddForce(f);
