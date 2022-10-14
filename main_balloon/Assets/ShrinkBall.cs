fddb568bc4ecae93f8a29774219021ed05b99058
Maria Jacobson <maria.jacobsons@icloud.com>
2022-10-14 14:45:42 +0200

    Bouncyballs and cactus

---
+++ main_balloon/Assets/ShrinkBall.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShrinkBall : MonoBehaviour
+{
+    public float timer = 0f;
+    public float shrinkTime = 6f;
+    public float minSize = 0.5f;
+
+    public bool isMinSize = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(isMinSize == false){StartCoroutine(Shrink());}
+    
+    }
+
+    private IEnumerator Shrink()
+    {
+        Vector2 startScale = transform.localScale;
+        Vector2 minScale = new Vector2(minSize, minSize);
+
+        do {
+            transform.localScale = Vector3.Lerp(startScale, minScale, timer / shrinkTime);
+            timer += Time.deltaTime;
+            yield return null;
+        }
+        while(timer < shrinkTime);
+        isMinSize = true;
+    }
+
+}
